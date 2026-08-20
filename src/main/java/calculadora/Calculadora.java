f0dca98ddf806b8a59d12113173c4c968a937ba1
Paulo Henrique Rosa Siqueira <112953437+paulohrsiqueira@users.noreply.github.com>
2026-08-20 20:54:02 -0300

    Implementa interface e entrada de números

--- src/main/java/calculadora/Calculadora.java
+++ src/main/java/calculadora/Calculadora.java
@@ -4,7 +4,10 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import java.awt.GridLayout;
-
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
 
 public class Calculadora {
 
@@ -15,8 +18,13 @@ public class Calculadora {
         janela.setTitle("Calculadora");
         janela.setSize(400, 600);
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+
+        JTextField visor = new JTextField();
 
         JPanel painel = new JPanel();
+        
+        
         painel.setLayout(new GridLayout(4, 4));
 
         String[] txt = {
@@ -30,10 +38,26 @@ public class Calculadora {
 
         for (int i = 0; i < 16; i++) {
             btn[i] = new JButton(txt[i]);
+
+            btn[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e ){
+                    JButton btnClicado = (JButton) e.getSource();
+                    
+                    String txtAtual = visor.getText();
+                    String txtProx = btnClicado.getText();
+
+                    visor.setText(txtAtual + txtProx);
+                }
+            });
+
+
             painel.add(btn[i]);
         }
 
-        janela.add(painel);
+   
+        janela.add(visor, BorderLayout.NORTH);
+        janela.add(painel, BorderLayout.CENTER);
 
         janela.setVisible(true);
     }
